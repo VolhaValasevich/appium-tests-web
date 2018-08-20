990377c88860cb5a4f09410b1e2e13a3c5af642f
Volha Valasevich <volha_valasevich@epam.com>
2018-08-20 15:44:41 +0300

    rewrite tests

--- test/spec.js
+++ test/spec.js
@@ -1,27 +1,27 @@
 const BASE_URL = 'https://www.onliner.by/';
-const DISABLE_NOTIFICATIONS = 'div.push-notification__close';
-const ITEM_UNDERLAY = 'a.header-style__underlay';
-const MENU_HEADER = 'div.header-style__title';
 
 describe('Onliner.by', () => {
 
     beforeAll(() => {
         browser.get(BASE_URL);
-        element(by.css(DISABLE_NOTIFICATIONS)).click();
+        element(by.css('div.push-notification__close')).click();
     });
 
-    it('must have correct title', () => {
-        expect(browser.getTitle()).toEqual('Onliner.by');
+    beforeEach(() => {
+        browser.get(BASE_URL);
     });
 
-    it('must have an item underlay', async () => {
-        const items = await element.all(by.css(ITEM_UNDERLAY));
-        expect(items.length).toEqual(1);
+    it('must open catalog', () => {
+        element(by.css('a.header-style__underlay')).click();
+        element(by.linkText("Каталог")).click();
+        expect(browser.getTitle()).toEqual('Каталог Onliner.by');
     });
 
-    it('must have a correct menu header', async () => {
-        await element(by.css(ITEM_UNDERLAY)).click();
-        const text = await element(by.css(MENU_HEADER)).getText();
-        expect(text).toEqual('Onliner');
+    it('must open tech news', () => {
+        element(by.css('a.header-style__underlay')).click();
+        element(by.linkText("Новости")).click();
+        element(by.linkText("Технологии")).click();
+        expect(browser.getTitle()).toEqual('Технологии onliner.by');
     });
+
 });
